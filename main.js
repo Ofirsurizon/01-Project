04a4739376bf86c01cc32fd818a02c66aa791432
ofir surizon <ofirsuri10@gmail.com>
2023-06-06 15:20:07 +0300

    Fixed, added delete button and fixed background note

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const images = []
+const IMAGES_KEY = "ofir-Gallery";
+let images = []
+loadFromLocalStorage()
+
+function saveToLocalStorage() {
+    const str = JSON.stringify(images);
+    localStorage.setItem(IMAGES_KEY, str);
+}
+
 
 function addTask() {
     event.preventDefault()
@@ -6,38 +14,63 @@ function addTask() {
     const taskNameBox = document.getElementById("taskNameBox");
     const taskTimeBox = document.getElementById("taskTimeBox");
     const taskDateBox = document.getElementById("taskDateBox");
-    const taskList = document.getElementById("taskList");
+
 
     const item = {
+        Image: '/assets/images/note.png',
         Task: taskNameBox.value,
         Time: taskTimeBox.value,
-        Date: taskDateBox.value,
-        Image: '/assets/images/notebg.png'
-    };
+        Date: taskDateBox.value
 
+    };
+    //push item to array
     images.push(item);
-    console.log(images);
+
+    //save to local storage
+    saveToLocalStorage();
+
+    // display the note
+    displayNotes();
+}
+
+function displayNotes() {
+    const taskList = document.getElementById("taskList");
 
     let html = "";
-    for (const item of images) {
+    for (let i = 0; i < images.length; i++) {
 
-        
         html += `
         <li>
-            <div class="note">
-                <img src="${item.Image}" alt="Task image">
-                <div class="note-text">
-                    Task: ${item.Task}, <br> Time: ${item.Time}, <br> Date: ${item.Date} <br>
-                    <button class="btn btn-danger delete-button">X</button>
-                    </div>
-                    
-                    
+            <div class="note text-center">
+                <p>Task: ${images[i].Task}</p>
+                <p>Time: ${images[i].Time}</p>
+                <p>Date: ${images[i].Date}</p>
+                <button id="${i}" class="btn btn-danger delete-button mx-auto d-block mb-2" onclick="deleteNote(this)">X</button>
             </div>
         </li>
-    `;
+        `;
 
     }
 
     taskList.innerHTML = html;
+}
+
+
+function deleteNote(element) {
+console.log(element);
+const index = element.id;
+images.splice(index,1)
+//save after delete
+saveToLocalStorage();
+//display after delete
+displayNotes();
+}
+
+function loadFromLocalStorage() {
+    const json = localStorage.getItem(IMAGES_KEY);
+    if (json != null && json.length > 0) {
+        images = JSON.parse(json);
+    }
+    displayNotes()
 
 }
